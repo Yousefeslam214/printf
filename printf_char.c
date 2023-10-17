c23467853b9f15ff3bedaecb74a532035dd67d3c
arwaa29 <arwahesham73@gmail.com>
2023-10-17 10:46:56 -0700

    done

--- printf_char.c
+++ printf_char.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
+/**
+ * _printf_char - takes argument
+ *
+ * @args: argument of char
+ *
+ * Return: 1
+*/
+
 int _printf_char(va_list args)
 {
 	_putchar(va_arg(args, int));
-	return(1);
+	return (1);
 }
