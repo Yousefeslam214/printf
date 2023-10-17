6ad9cf913ceda63c133f3dbb2af8c2a64757dd1f
arwaa29 <arwahesham73@gmail.com>
2023-10-17 10:51:50 -0700

    done

--- printf_persentage.c
+++ printf_persentage.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
+/**
+ * _printf_persentage - take argument and print % by putchar
+ *
+ * @args: for argument
+ * Return: 1
+*/
+
 int _printf_persentage(va_list args)
 {
 	(void)args;
+
 	_putchar('%');
-	return(1);
+	return (1);
 }
